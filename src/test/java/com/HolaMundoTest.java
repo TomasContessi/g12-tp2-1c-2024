f7250d66217de916643e6b75a7bad93d4ed7b78f
tomas.contessi <tomas.contessi@iquall.net>
2024-06-01 23:11:58 -0300

    prueba para ver que pasa cuando un test falla

--- src/test/java/com/HolaMundoTest.java
+++ src/test/java/com/HolaMundoTest.java
@@ -9,6 +9,6 @@ public class HolaMundoTest {
     @Test
     public void testSaludar() {
         HolaMundo holaMundo = new HolaMundo();
-        assertEquals("Hola Mundo", holaMundo.saludar());
+        assertEquals("Hola Mundos", holaMundo.saludar());
     }
 }
