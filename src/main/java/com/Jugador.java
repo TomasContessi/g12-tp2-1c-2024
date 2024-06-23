41f552b21f22166a0f283e14124ff34c38c20ab6
ZambranoCarlos <134104840+ZambranoCarlos@users.noreply.github.com>
2024-06-23 19:57:14 -0300

    Agrega una opcion, borra una opcion y borra todas las opciones, responde.

--- src/main/java/com/Jugador.java
+++ src/main/java/com/Jugador.java
@@ -7,23 +7,37 @@ public class Jugador {
     private int puntos;
     private ArrayList<Multiplicador> multiplicadores;
     //private Anulador anulador;
-    //private Exclusividad exclusividad;
+    private Exclusividad exclusividad;
+    int opcionElegida;
+    ArrayList<Opcion> respuesta = new ArrayList<Opcion>();
 
     Jugador(String nombre){
         this.name = nombre;
+        this.puntos = 0;
+        this.multiplicadores = new ArrayList<>();
+        this.exclusividad = new Exclusividad();
+        this.opcionElegida = 0;
+        this.respuesta = new ArrayList<>();
     }
 
-    public ArrayList<Opcion> responderPregunta(Pregunta pregunta){
-        ArrayList<Opcion> respuesta = new ArrayList<Opcion>();
-        //this.elegirOpcion(pregunta);
-      //  pregunta.setMultiplicador(multiplicadores.get(0));
+    public ArrayList<Opcion> responderPregunta(){
         return respuesta;
     }
-
-    void elegirOpcion(ArrayList<Opcion> opcionesAElegir,ArrayList<Opcion> opcionesElegidas){
-        opcionesElegidas.add(opcionesAElegir.get(0));
+    void eliminarOpciones(){
+        this.respuesta.clear();
     }
-
+    void eliminarOpcion(){
+        this.respuesta.remove(opcionElegida);
+    }
+    void agregarOpcion(Pregunta pregunta){
+        this.respuesta.add(pregunta.getOpcion(opcionElegida));
+    }
+    void elegirOpcion(int opcionElegida){
+        this.opcionElegida = (opcionElegida-1);
+    }
+    int opcionesElegidas(){
+        return respuesta.size();
+    }
     public void asignarPuntos(int puntos){
         this.puntos += puntos;
     }
--- src/test/java/com/JugadorTest.java
+++ src/test/java/com/JugadorTest.java
@@ -2,6 +2,8 @@ package com;
 
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //----------------------------------------------------------------------------------------------------------------------------------------------
@@ -10,27 +12,99 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JugadorTest {
     @Test
-    public void test01PreguntaMultipleChoiceConPenalidadRespondeCorrectamente(){
+    public void test01AgregaTresOpciones(){
 
         //Arrange
         Tipo tipo = new MultipleChoiceConPenalidad();
         ArrayList<Opcion> opciones = new ArrayList<>();
         ArrayList<Opcion> respuestaCorrecta = new ArrayList<>();
-        ArrayList<Opcion> respuestaObtenida = new ArrayList<>();
-        ArrayList<Opcion> respuestaEsperada = new ArrayList<>();
         opciones.add(new OpcionString("Messi"));
         opciones.add(new OpcionString("Maradona"));
         opciones.add(new OpcionString("Cristiano"));
-        respuestaCorrecta.add(new OpcionString("1"));
-        respuestaEsperada.add(new OpcionString("1"));
+        respuestaCorrecta.add(new OpcionString("Messi"));
+        int opcionesElegidasEsperadas = 3;
+        int opcionesElegidas = 0;
         String enunciado = "Jugador con mas balones de oro";
         String tema = "Futbol";
 
         Jugador jugador = new Jugador("Joel");
         Pregunta pregunta = new Pregunta(tipo,respuestaCorrecta,enunciado,opciones,tema);
         //Act
-        respuestaObtenida = jugador.responderPregunta(pregunta);
+        jugador.elegirOpcion(1);
+        jugador.agregarOpcion(pregunta);
+        jugador.elegirOpcion(2);
+        jugador.agregarOpcion(pregunta);
+        jugador.elegirOpcion(3);
+        jugador.agregarOpcion(pregunta);
+        opcionesElegidas = jugador.opcionesElegidas();
         //Assert
-        assertEquals(respuestaEsperada,respuestaObtenida);
+        assertEquals(opcionesElegidasEsperadas,opcionesElegidas);
     }
+
+    @Test
+    public void test02AgregaTresOpcionesYEliminaUna(){
+
+        //Arrange
+        Tipo tipo = new MultipleChoiceConPenalidad();
+        ArrayList<Opcion> opciones = new ArrayList<>();
+        ArrayList<Opcion> respuestaCorrecta = new ArrayList<>();
+        opciones.add(new OpcionString("Messi"));
+        opciones.add(new OpcionString("Maradona"));
+        opciones.add(new OpcionString("Cristiano"));
+        respuestaCorrecta.add(new OpcionString("Messi"));
+        respuestaCorrecta.add(new OpcionString("Maradona"));
+        int opcionesElegidasEsperadas = 2;
+        int opcionesElegidas = 0;
+        String enunciado = "Jugadores de la seleccion Argentina";
+        String tema = "Futbol";
+
+        Jugador jugador = new Jugador("Joel");
+        Pregunta pregunta = new Pregunta(tipo,respuestaCorrecta,enunciado,opciones,tema);
+        //Act
+        jugador.elegirOpcion(1);
+        jugador.agregarOpcion(pregunta);
+        jugador.elegirOpcion(2);
+        jugador.agregarOpcion(pregunta);
+        jugador.elegirOpcion(3);
+        jugador.agregarOpcion(pregunta);
+        jugador.eliminarOpcion();
+        opcionesElegidas = jugador.opcionesElegidas();
+        //Assert
+       assertEquals(opcionesElegidasEsperadas,opcionesElegidas);
+
+    }
+    @Test
+    public void test03AgregaTresOpcionesEliminaUnaYBorraTodasLasOpciones(){
+
+        //Arrange
+        Tipo tipo = new MultipleChoiceConPenalidad();
+        ArrayList<Opcion> opciones = new ArrayList<>();
+        ArrayList<Opcion> respuestaCorrecta = new ArrayList<>();
+        opciones.add(new OpcionString("Messi"));
+        opciones.add(new OpcionString("Maradona"));
+        opciones.add(new OpcionString("Cristiano"));
+        respuestaCorrecta.add(new OpcionString("Messi"));
+        respuestaCorrecta.add(new OpcionString("Maradona"));
+        int opcionesElegidasEsperadas = 0;
+        int opcionesElegidas = 0;
+        String enunciado = "Jugadores de la seleccion Argentina";
+        String tema = "Futbol";
+
+        Jugador jugador = new Jugador("Joel");
+        Pregunta pregunta = new Pregunta(tipo,respuestaCorrecta,enunciado,opciones,tema);
+        //Act
+        jugador.elegirOpcion(1);
+        jugador.agregarOpcion(pregunta);
+        jugador.elegirOpcion(2);
+        jugador.agregarOpcion(pregunta);
+        jugador.elegirOpcion(3);
+        jugador.agregarOpcion(pregunta);
+        jugador.eliminarOpcion();
+        jugador.eliminarOpciones();
+        opcionesElegidas = jugador.opcionesElegidas();
+        //Assert
+        assertEquals(opcionesElegidasEsperadas,opcionesElegidas);
+
+    }
+
 }
