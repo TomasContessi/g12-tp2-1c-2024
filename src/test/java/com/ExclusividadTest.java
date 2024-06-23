b3e3f2c6f84570295f4c069255f19fed8b0a1319
ZambranoCarlos <134104840+ZambranoCarlos@users.noreply.github.com>
2024-06-23 18:25:18 -0300

    Tiene un getOpcion

---
+++ src/test/java/com/ExclusividadTest.java
@@ -0,0 +1,63 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExclusividadTest {
+    @Test
+    public void test01UnJugadorUsaExclusividadYSoloUnoAcierta(){
+        //Arrange
+        Exclusividad exclusividad = new Exclusividad();
+        int cantidadJugadoresAciertan = 1;
+        int jugadoresUsaronExclusividad = 1;
+        int multiplicadorPuntosEsperados = 2;
+        int multiplicadorPuntosObtenidos = 0;
+        //Act
+        multiplicadorPuntosObtenidos = exclusividad.calcularExclusividad(jugadoresUsaronExclusividad,cantidadJugadoresAciertan);
+        //Assert
+        assertEquals(multiplicadorPuntosEsperados,multiplicadorPuntosObtenidos);
+    }
+
+    @Test
+    public void test02TresJugadoresUsanExclusividadYSoloUnoAcierta(){
+        //Arrange
+        Exclusividad exclusividad = new Exclusividad();
+        int cantidadJugadoresAciertan = 1;
+        int jugadoresUsaronExclusividad = 3;
+        int multiplicadorPuntosEsperados = 6;
+        int multiplicadorPuntosObtenidos = 0;
+        //Act
+        multiplicadorPuntosObtenidos = exclusividad.calcularExclusividad(jugadoresUsaronExclusividad,cantidadJugadoresAciertan);
+        //Assert
+        assertEquals(multiplicadorPuntosEsperados,multiplicadorPuntosObtenidos);
+    }
+
+    @Test
+    public void test03UnJugadorUsaExclusividadYDosAciertan(){
+        //Arrange
+        Exclusividad exclusividad = new Exclusividad();
+        int cantidadJugadoresAciertan = 2;
+        int jugadoresUsaronExclusividad = 1;
+        int multiplicadorPuntosEsperados = 0;
+        int multiplicadorPuntosObtenidos = 0;
+        //Act
+        multiplicadorPuntosObtenidos = exclusividad.calcularExclusividad(jugadoresUsaronExclusividad,cantidadJugadoresAciertan);
+        //Assert
+        assertEquals(multiplicadorPuntosEsperados,multiplicadorPuntosObtenidos);
+    }
+
+    @Test
+    public void test04DosJugadoresUsanExclusividadYTresAciertan(){
+        //Arrange
+        Exclusividad exclusividad = new Exclusividad();
+        int cantidadJugadoresAciertan = 3;
+        int jugadoresUsaronExclusividad = 2;
+        int multiplicadorPuntosEsperados = 0;
+        int multiplicadorPuntosObtenidos = 0;
+        //Act
+        multiplicadorPuntosObtenidos = exclusividad.calcularExclusividad(jugadoresUsaronExclusividad,cantidadJugadoresAciertan);
+        //Assert
+        assertEquals(multiplicadorPuntosEsperados,multiplicadorPuntosObtenidos);
+    }
+}
