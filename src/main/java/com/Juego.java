840782b8891a4e31417315140f96117b1ad8bdec
tomas.contessi <tomas.contessi@iquall.net>
2024-06-23 22:58:14 -0300

    un poco de avance en juego

--- src/main/java/com/Juego.java
+++ src/main/java/com/Juego.java
@@ -2,14 +2,36 @@ package com;
 
 import java.util.ArrayList;
 import java.util.Random;
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------------------------------
 public class Juego {
     private ArrayList<Jugador> jugadores;
     private String preguntasPath;
     private DiccionarioPreguntas diccionarioPreguntas;
     private String ultimaTematica;
+    private Pregunta preguntaTurno;
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
 
     public void cargarPregunta(){
-        Random random = new Random();
-        int id = random.nextInt(24);
+    }
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+    public void agregarJugador(Jugador jugador){
+        this.jugadores.add(jugador);
+    }
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+    public void setPath(String preguntasPath){
+    this.preguntasPath = preguntasPath;
+}    
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+    public void cargarPreguntas(DiccionarioPreguntas diccionarioPreguntas){
+        this.diccionarioPreguntas = diccionarioPreguntas;
+        this.diccionarioPreguntas.asignarPreguntasPorTema(this.preguntasPath);
     }
 }
