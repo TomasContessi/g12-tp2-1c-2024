51c11ade25e75efaece3a3ead96384efacb01e9c
ZambranoCarlos <134104840+ZambranoCarlos@users.noreply.github.com>
2024-06-26 19:30:46 -0300

    Crea una escena con un layout

---
+++ src/main/java/com/Escena.java
@@ -0,0 +1,19 @@
+package com;
+
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Escena {
+    Scene escena;
+
+    public Escena(Parent parent){
+        this.escena = new Scene(parent);
+    }
+    Scene getEscena(){
+        return this.escena;
+    }
+    void setEscena(javafx.scene.layout.StackPane layout){
+        Scene scene = new Scene(layout);
+    }
+}
