9cd9e14c57d6201431803aed1dcdb5f07fad4a51
TomasContessi <tomascontessi2@hotmail.com>
2024-06-27 22:50:43 -0300

    acomodado el modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
-    requires javafx.graphics;
 
     exports com; // Ajusta el paquete a tu estructura de paquetes real
 }
