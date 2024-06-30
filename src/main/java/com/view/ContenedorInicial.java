ebfbf1c30cb5c91c95d4b030269da059e5e0ea20
ZambranoCarlos <134104840+ZambranoCarlos@users.noreply.github.com>
2024-06-30 13:05:51 -0300

    Escena de arranque del juego

---
+++ src/main/java/com/view/ContenedorInicial.java
@@ -0,0 +1,20 @@
+package com.view;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ContenedorInicial extends VBox {
+    Stage stage;
+    public ContenedorInicial(Stage stage, Scene proximaEscena) {
+
+        super();
+        this.stage = stage;
+        Button boton = new Button();
+        boton.setText("Comenzar juego");
+        //boton.setOnAction();
+
+    }
+
+}
