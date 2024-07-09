dff176d1c85c691853798b6f1dd0806064312a83
ZambranoCarlos <134104840+ZambranoCarlos@users.noreply.github.com>
2024-07-09 15:55:58 -0300

    cambios

--- src/main/java/com/view/ContenedorInicial.java
+++ src/main/java/com/view/ContenedorInicial.java
@@ -20,19 +20,14 @@ public class ContenedorInicial extends VBox {
         this.setAlignment(Pos.CENTER);
         this.setSpacing(20);
         //this.setPadding(new Insets(20));
-        Button boton = new Button();
         Label titulo = new Label();
         titulo.setText("Ingresar cantidad de jugadores");
         TextField textField = new TextField();
-        boton.setText("Continuar");
 
-        textField.setOnAction(e -> {
-            int cantidadJugadores = Integer.parseInt(textField.getText());
-        });
 
-        this.getChildren().addAll(titulo,textField,boton);
-        CambiarEscenaEventHandler botonContinuar = new CambiarEscenaEventHandler(stage, proximaEscena);
-        boton.setOnAction(botonContinuar);
+        this.getChildren().addAll(titulo,textField);
+        GuardarTextoEventHandler guardarTexto = new GuardarTextoEventHandler(stage, proximaEscena);
+        textField.setOnAction(guardarTexto);
 
     }
 
