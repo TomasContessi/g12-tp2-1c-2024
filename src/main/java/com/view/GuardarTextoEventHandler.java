ccb20aa8f75668adbca3f15c39513a97d596e28f
ZambranoCarlos <134104840+ZambranoCarlos@users.noreply.github.com>
2024-07-09 15:52:06 -0300

    cambios

--- src/main/java/com/view/GuardarTextoEventHandler.java
+++ src/main/java/com/view/GuardarTextoEventHandler.java
@@ -3,13 +3,20 @@ package com.view;
 import com.model.jugador.Jugador;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class GuardarTextoEventHandler implements EventHandler<ActionEvent> {
     TextField textField;
+    String texto;
+    Stage stage;
+    Scene proximaEscena;
 
-    public GuardarTextoEventHandler(){
+    public GuardarTextoEventHandler(Stage stage, Scene proximaEscena){
         this.textField = new TextField();
+        this.stage = stage;
+        this.proximaEscena = proximaEscena;
     }
     /*
     public Jugador guardarJugador(){
@@ -17,6 +24,8 @@ public class GuardarTextoEventHandler implements EventHandler<ActionEvent> {
     }*/
     @Override
     public void handle(ActionEvent actionEvent) {
-        String texto = textField.getText();
+         texto = textField.getText();
+         stage.setScene(proximaEscena);
+
     }
 }
